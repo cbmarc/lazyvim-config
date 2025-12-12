12e1e3c6a3e238770f9e5be635b62236a0dd93bf
Marc Carné <marc.carne.bori@gmail.com>
2025-12-12 22:43:01 +0100

    Update config

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        menu = {
+          auto_show = true,
+          auto_show_delay_ms = 250,
+        },
+        ghost_text = { enabled = false },
+      },
+    },
+  },
+}
